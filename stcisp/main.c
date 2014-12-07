97d8345c0741257d0e56c91c53ccb553e81a8ddf
Mario Huang <guiyang.huang@gmail.com>
2014-12-07 21:16:45 +0800

    update LICENSE

--- stcisp/main.c
+++ stcisp/main.c
@@ -1,4 +1,7 @@
-﻿/*  Copyright (C) 2014 Mario Huang  This program is free software: you can redistribute it and/or modify
+/*
+Copyright (C) 2014 Mario Huang
+
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -9,4 +12,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.  */  #include "isp.h"  int main(int argc, char *argv[]) { 	return isp_main(argc, argv); }
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "isp.h"
+
+int main(int argc, char *argv[])
+{
+	return isp_main(argc, argv);
+}
